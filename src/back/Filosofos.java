20e75e1266a931e7ab06126ba35e8273ed933bba
Bonnielamb6 <edgartrabajos2205@gmail.com>
2023-09-02 22:23:37 -0600

    Inicio

---
+++ src/back/Filosofos.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package back;
+
+/**
+ *
+ * @author PC
+ */
+public class Filosofos {
+    boolean comiendo;
+    int tiempoEspera;
+    Planificador buffer;
+    
+    
+    public Filosofos(Planificador b, int t){
+        comiendo = false;
+        tiempoEspera = t;
+        buffer = b;
+    }
+    
+    
+    public void comer(){
+    
+    }
+    
+    public void tomarIzquierda(){
+    
+    }
+    
+    public void tomarDerecha(){}
+    
+    public void empezarComer(){
+        comiendo = true;
+    }
+    
+    public void terminarComer(){
+        comiendo = false;
+    }
+}
